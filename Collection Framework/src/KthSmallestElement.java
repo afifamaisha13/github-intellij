fe689c87ddf01eb670c12be9586fe4a9f8b783cd
afifamaisha13 <afifamaisha13@gmail.com>
2025-05-27 18:50:16 +0600

    Update KthSmallestElement.java

--- Collection Framework/src/KthSmallestElement.java
+++ Collection Framework/src/KthSmallestElement.java
@@ -1,15 +1,31 @@
-// Finds the kth smallest element from an ArrayList
 import java.util.*;
 
 public class KthSmallestElement {
-    public static int findKthSmallest(List<Integer> list, int k) {
+
+    public static int findKthSmallest(ArrayList<Integer> list, int k) {
+       
+        // Sort the ArrayList
         Collections.sort(list);
-        return list.get(k - 1); // index starts at 0
+
+        // Return the k-th smallest element (1-based indexing)
+        return list.get(k - 1);
     }
 
     public static void main(String[] args) {
-        List<Integer> numbers = new ArrayList<>(Arrays.asList(7, 2, 5, 3, 9, 1));
-        int k = 3;
-        System.out.println(k + "rd smallest: " + findKthSmallest(numbers, k));
+        Scanner sc = new Scanner(System.in);
+
+        ArrayList<Integer> list = new ArrayList<>();
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            list.add(sc.nextInt());
+        }
+
+        int k = sc.nextInt();
+
+            int smallest = findKthSmallest(list, k);
+            System.out.println("The " + k + "-th smallest element is: " + smallest);
+        
+        sc.close();
     }
 }
